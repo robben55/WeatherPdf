6a99c61c248430fedd5d46bca015d80ad4596507
robben <email@email.com>
2024-10-10 02:31:51 +0500

    fixed monthly-report api logic

--- WeatherPdf/Program.cs
+++ WeatherPdf/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using QuestPDF.Fluent;
 using QuestPDF.Infrastructure;
 using System.Globalization;
@@ -7,7 +6,6 @@ using WeatherPdf.Database.Context;
 using WeatherPdf.DependencyInjection;
 using WeatherPdf.Pdf.Weather.ContentModels;
 using WeatherPdf.Services;
-using WeatherPdf.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 QuestPDF.Settings.License = LicenseType.Community;
@@ -20,22 +18,26 @@ var app = builder.Build();
 app.MapGet("monthly-report", async (ApplicationContext context, IGeneratePdf pdf) =>
 {
     var currentYear = DateTime.Now.Year;
-    var month = DateTime.Now.Month;
+    var month = DateTime.Now.Month - 1;
+    if (month == 0)
+    {
+        currentYear--;
+        month = 12;
+    }
     var end = DateTime.DaysInMonth(currentYear, month);
-
     var startDateTime = new DateTime(currentYear, month, 1);
     var endDateTime = new DateTime(currentYear, month, end);
-
     var weatherReport = await context.WeatherDatas.Where(x => x.SearchedTime >= startDateTime && x.SearchedTime <= endDateTime).ToListAsync();
     var forHeader = new HeaderContent()
     {
         From = "1",
         To = end.ToString(),
-        Month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(DateTime.Now.Month)
+        Month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(month)
     };
     var content = pdf.CreatePdf(weatherReport, forHeader).GeneratePdf();
     return Results.File(content, "application/pdf", "weather-report.pdf");
 });
+
 
 if (app.Environment.IsDevelopment())
 {
